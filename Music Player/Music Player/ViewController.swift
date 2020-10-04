ccfedd2ac46706209848ac6d8f705d8c895ffb89
Vijay Parmar <vspxcoder@gmail.com>
2020-10-04 10:10:39 +0530

    Added UI Design

---
+++ Music Player/Music Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by Vijay Parmar on 04/10/20.
+//  Copyright © 2020 Dashu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
