a79b3c3770536a00349fa6ce85138134d06f936a
Vijay Parmar <vspxcoder@gmail.com>
2020-10-05 09:43:48 +0530

    Added Audio Player

--- Music Player/Music Player/ViewController.swift
+++ Music Player/Music Player/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnplay: UIButton!
+    var audio = AVAudioPlayer()
+    var isPlay = false
+    var str:String?
+    var img:UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let path = Bundle.main.path(forResource: "ganesha", ofType: "mp3")
+        let url = URL(fileURLWithPath: path!)
+        do {
+             audio = try    AVAudioPlayer(contentsOf: url)
+        } catch  {
+            print("path not found")
+            
+        }
     }
 
-
+    @IBAction func btn_click(_ sender: Any) {
+        
+        if isPlay
+        {
+            img = UIImage(named: "play")
+            audio.pause()
+            
+        }
+        else{
+             img = UIImage(named: "pause")
+                audio.play()
+        }
+        btnplay.setImage(img, for: .normal)
+        isPlay = !isPlay
+        
+        
+        print(isPlay)
+    }
+    
 }
 
